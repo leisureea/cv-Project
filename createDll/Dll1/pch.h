6a559d0b93116f20701fcf28afcb0b0a165d98b1
leisureea <45066301+leisureea@users.noreply.github.com>
2020-05-22 08:22:54 +0800

    Create pch.h

---
+++ createDll/Dll1/pch.h
@@ -0,0 +1,27 @@
+﻿#ifndef PCH_H_
+#define PCH_H_
+
+#include<iostream>
+#include<opencv2/highgui.hpp>
+using namespace std;
+using namespace cv;
+
+#ifdef SERVERDLL_EXPORTS
+#define SERVERDLL_API __declspec(dllexport)
+#else
+#define SERVERDLL_API __declspec(dllimport)
+#endif
+
+class SERVERDLL_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+	void show_img();
+private:
+
+};
+
+SERVERDLL_API void func();
+
+#endif
