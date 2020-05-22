4f39340703261593b5283ba99656e036d1253380
leisureea <45066301+leisureea@users.noreply.github.com>
2020-05-22 08:22:56 +0800

    Create pch.cpp

---
+++ createDll/Dll1/pch.cpp
@@ -0,0 +1,24 @@
+﻿// pch.cpp: 与预编译标头对应的源文件
+#include "pch.h"
+
+
+MyClass::MyClass() {}
+MyClass::~MyClass() {}
+
+void MyClass::show_img()
+{
+    std::cout << "1" << endl;
+    Mat img = imread("1.jpg", 1);
+    if (img.empty())
+    {
+        std::cout << "图片打开失败" << endl;
+    }
+    imshow("he", img);
+    waitKey(0);
+}
+
+void func()
+{
+    cout << "2" << endl;
+}
+
